385d967401cd2ef61601cd641aeef628e892637c
Igor Popov <ipopov4422@gmail.com>
2024-06-07 10:47:05 +0100

    Add multiple files

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -2,6 +2,7 @@
 
 import logging
 import argparse
+import requests
 
 HEADERS = ['cookie', 'timestamp']
 DEFAULT_LOG_FILE = 'most_active_cookie.log'
@@ -33,8 +34,9 @@ class AppLogger:
 
 
 class CookieLogProcessor:
-    def __init__(self, filename, log_level=None, logger=None):
+    def __init__(self, filename, log_file_list=None, log_level=None, logger=None):
         self.filename = filename
+        self.log_file_list = log_file_list
         self._filtered_cookies = {}
         if logger is None:
             log_config = AppLogger(name=__name__, log_level=log_level)
@@ -44,35 +46,90 @@ class CookieLogProcessor:
         self.logger.info(
             f'Initialized CookieLogProcessor with file: {self.filename}')
 
-    def read_log_file(self):
-        self.logger.info(f'Reading log file: {self.filename}')
-        self.cookeis = []
-
-        with open(self.filename, 'r') as file:
+    def __read_single_file(self, filename):
+        self.logger.info(f'Reading log file: {filename}')
+        cookies = []
+        with open(filename, 'r') as file:
             for i, line in enumerate(file):
                 if i == 0:
-                    self.cookie_log_header = line.strip().split(',')
-                    if len(self.cookie_log_header) != len(HEADERS):
+                    cookie_log_header = line.strip().split(',')
+                    if len(cookie_log_header) != len(HEADERS):
                         raise ValueError(
                             'Wrong log file format. Header columns do not match the expected format. Header format should be string: cookie,timestamp')
                     for i, header_column in enumerate(HEADERS):
-                        if header_column != self.cookie_log_header[i]:
+                        if header_column != cookie_log_header[i]:
                             raise ValueError(
                                 f'Wrong log file format. Header column \'{header_column}\' not found in the file. Header format should be string: cookie,timestamp')
                 else:
                     cookie_line = line.strip()
                     if cookie_line:
-                        self.cookeis.append(cookie_line.split(','))
-
-        if len(self.cookeis) == 0:
+                        cookies.append(cookie_line.split(','))
+        if len(cookies) == 0:
             raise ValueError('No cookies found in the log file')
-        self.logger.info(
-            f'Read {len(self.cookeis)} cookies from file')
+        return cookies
+
+    def __read_multiple_files(self, log_file_list):
+        def check_file_source(filename):
+            if filename.startswith('http://') or filename.startswith('https://'):
+                return 'remote'
+            else:
+                return 'local'
+
+        cookies = []
+        for url in log_file_list:
+            if check_file_source(url) == 'remote':
+                response = requests.get(url)
+                if response.status_code == 200:
+                    raw_cookies_file = response.text.strip().split('\n')
+                    clear_cookies = [cookie.split(',')
+                                     for cookie in raw_cookies_file[1:]]
+                    print('raw_cookies_file: ', clear_cookies)
+                    cookies += clear_cookies
+
+                else:
+                    raise ValueError(
+                        f'Failed to fetch log file from URL: {url}')
+            else:
+                cookies += self.__read_single_file(url)
+
+        return cookies
+
+    def read_log_file(self):
+
+        if self.log_file_list:
+            self.cookeis = self.__read_multiple_files(self.log_file_list)
+        else:
+            self.cookeis = self.__read_single_file(self.log_file)
+
+        # self.logger.info(f'Reading log file: {self.filename}')
+        # self.cookeis = []
+
+        # with open(self.filename, 'r') as file:
+        #     for i, line in enumerate(file):
+        #         if i == 0:
+        #             self.cookie_log_header = line.strip().split(',')
+        #             if len(self.cookie_log_header) != len(HEADERS):
+        #                 raise ValueError(
+        #                     'Wrong log file format. Header columns do not match the expected format. Header format should be string: cookie,timestamp')
+        #             for i, header_column in enumerate(HEADERS):
+        #                 if header_column != self.cookie_log_header[i]:
+        #                     raise ValueError(
+        #                         f'Wrong log file format. Header column \'{header_column}\' not found in the file. Header format should be string: cookie,timestamp')
+        #         else:
+        #             cookie_line = line.strip()
+        #             if cookie_line:
+        #                 self.cookeis.append(cookie_line.split(','))
+
+        # if len(self.cookeis) == 0:
+        #     raise ValueError('No cookies found in the log file')
+        # self.logger.info(
+        #     f'Read {len(self.cookeis)} cookies from file')
 
     def __filter_and_count_cookies(self, target_date):
         self.logger.info(
             f'Filtering and counting cookies for date: {target_date}')
         for line in self.cookeis:
+            # print('line: ', line)
             cookie, timestamp = line
             if target_date in timestamp:
                 self._filtered_cookies[cookie] = self._filtered_cookies.get(
@@ -97,7 +154,9 @@ class CookieLogCLI:
         self.parser = argparse.ArgumentParser(
             description='Find the most active cookie in a log file for a specific day.')
         self.parser.add_argument(
-            '-f', '--file', required=True, help='Path to the cookie log file')
+            '-f', '--file', help='Path to the cookie log file')
+        self.parser.add_argument(
+            '--file_list', help='File list of cookies separated by comma')
         self.parser.add_argument('-d', '--date', required=True,
                                  help='Date (YYYY-MM-DD) to find the most active cookie')
         self.parser.add_argument('--log-level', type=str, default='NOTSET',
@@ -113,6 +172,9 @@ class CookieLogCLI:
         self.date = self.args.date
         self.log_level = self.args.log_level
         self.log_file = self.args.log_file
+        print(self.args.file_list)
+        self.log_file_list = self.args.file_list.split(
+            ',') if self.args.file_list else None
 
         if self.logger is None:
             local_log_config = AppLogger(
@@ -125,17 +187,17 @@ class CookieLogCLI:
         self.logger.info("Provided cli arguments: %s", arguments_string)
 
     def run(self):
-        try:
-            processor = CookieLogProcessor(
-                self.file, self.log_level, self.logger)
-            processor.read_log_file()
-            most_active_cookies = processor.find_most_active_cookies(
-                self.date)
+        # try:
+        processor = CookieLogProcessor(
+            self.file, self.log_file_list, self.log_level, self.logger)
+        processor.read_log_file()
+        most_active_cookies = processor.find_most_active_cookies(
+            self.date)
 
-            for cookie in most_active_cookies:
-                print(cookie)
-        except Exception as e:
-            self.logger.error("An error occurred: %s", e)
+        for cookie in most_active_cookies:
+            print(cookie)
+        # except Exception as e:
+        #     self.logger.error("An error occurred: %s", e)
 
 
 if __name__ == "__main__":
